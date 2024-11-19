9d64b677b0ca6f65cc6b87cdae11cbb88cbdac5b
Sergi <100451218@alumnos.uc3m.es>
2024-11-19 23:36:29 +0100

    Responsive images and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,19 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
+      <div className="footer-content">
+        <ul className="contact-links">
+          <li><a href="https://linkedin.com/in/sergi-vila-marin" target="_blank" rel="noreferrer">LinkedIn</a></li>
+          <li><a href="https://github.com/100451218" target="_blank" rel="noreferrer">GitHub</a></li>
+          <li><a href="https://drive.google.com/file/d/1aMcnA2doclezht_MZaCpyAVMdehQnThh/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a></li>
+        </ul>
+        <div className="contact-info">
+          <p>+34 629 50 55 54</p>
+          <p>sergivi9@gmail.com</p>
+        </div>
+      </div>
+        
       
-      <ul className="social-links">
-        <li><a href="https://linkedin.com/in/sergi-vila-marin" target="_blank" rel="noreferrer">LinkedIn</a></li>
-        <li><a href="https://github.com/100451218" target="_blank" rel="noreferrer">GitHub</a></li>
-        <li><a href="https://drive.google.com/file/d/1aMcnA2doclezht_MZaCpyAVMdehQnThh/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a></li>
-        <li id="Phone"><p>Phone: +34 629 50 55 54  &nbsp; &nbsp;&nbsp;Email: sergivi9@gmail.com</p></li>
-      </ul>
     </footer>
   );
 }
