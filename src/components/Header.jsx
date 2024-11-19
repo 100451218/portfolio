49d4dca98bbc7f98ba9ab26c95e00219036654fc
Sergi <100451218@alumnos.uc3m.es>
2024-11-19 11:58:54 +0100

    Header format

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ function Header (){
     return (
         <header className="header">
             <nav className="navbar">
-                <h1>Sergi Vila</h1>
-                <ul>
+                <h1 className="logo">Sergi Vila</h1>
+                <ul className="nav-links">
                     <li><Link to="/">Inicio</Link></li>
                     <li><Link to="/about">About</Link></li>
                 </ul>
